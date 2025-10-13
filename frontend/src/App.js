90ec461209b58211651fbc816b2f32caab109820
Jana <janarademan1236@gmail.com>
2025-10-13 16:13:56 +0200

    downloaded neccesary libs, and added a dashboard page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
@@ -19,6 +20,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
