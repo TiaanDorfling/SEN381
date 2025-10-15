079379e6cfc7a608a5e6a91338dc43152527172b
Jana <janarademan1236@gmail.com>
2025-10-15 12:57:13 +0200

    home page skeleton kinda finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "./App.css";
-//pages
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/LogIn";
-import Forum from "./pages/Forum";
-import courses from "./pages/courses";
+
 
 function App() {
-   return (
-    <Router>
-      <div className="App">
-        {/* navbar */}
-        <nav className="navbar">
-          <h2>BC Learning Hub</h2>
-          <ul>
-            <li>Home</li>
-            <li>Login / Register</li>
-            <li>Forum</li>
-            <li>Courses</li>
-          </ul>
-        </nav>
-
-        {/* Page Routes */}
-        {/* <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="//LogIn" element={<Login />} />
-          <Route path="/Forum" element={<Forum />} />
-          <Route path="/courses" element={<Courses />} />
-        </Routes> */}
-      </div>
-    </Router>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
