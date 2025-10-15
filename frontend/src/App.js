ed578cdc258505907fa2707081bd1fc69fb7940d
Jana <janarademan1236@gmail.com>
2025-10-15 12:23:59 +0200

    busy with the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,36 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+//pages
 import Home from "./pages/Home";
-import LogIn from "./auth/LogIn";
-import SignUp from "./auth/SignUp";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/LogIn";
 import Forum from "./pages/Forum";
-import Messages from "./pages/Messages";
-
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import courses from "./pages/courses";
 
 function App() {
-  return (
-    <BrowserRouter>
-      
-      <main className="container mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/forum" element={<Forum />} />
-          <Route path="/messages" element={<Messages />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-    
+   return (
+    <Router>
+      <div className="App">
+        {/* navbar */}
+        <nav className="navbar">
+          <h2>BC Learning Hub</h2>
+          <ul>
+            <li>Home</li>
+            <li>Login / Register</li>
+            <li>Forum</li>
+            <li>Courses</li>
+          </ul>
+        </nav>
+
+        {/* Page Routes */}
+        {/* <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="//LogIn" element={<Login />} />
+          <Route path="/Forum" element={<Forum />} />
+          <Route path="/courses" element={<Courses />} />
+        </Routes> */}
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,36 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome Home</h1>
-    </div>
+    
+     <div className="Home">
+      {/* Navbar Section */}
+      <section>
+        <h3>Overview</h3>
+        <p>
+
+        </p>
+      </section>
+
+      <section>
+        <h3>Your Progress</h3>
+        <p>
+
+        </p>
+      </section>
+
+      <section>
+        <h3>Completed Courses</h3>
+        <p>
+
+        </p>
+      </section>
+      <section>
+        <h3>Your Grades</h3>
+        <p>
+
+        </p>
+      </section>
+      </div>
   );
 };
 
